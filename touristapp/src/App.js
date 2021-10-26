a575015c4218c444bd354d863868c9d360509183
erhan11 <77719565+erhan11@users.noreply.github.com>
2021-10-26 14:20:55 +0300

    first commit

---
+++ touristapp/src/App.js
@@ -0,0 +1,20 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
+
+=======
+import './App.css';
+>>>>>>> master
+
+=======
+>>>>>>> 320dbb085a3a993802e6cff5bc9015ced0a7bd27
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>Tourist App</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ touristapp/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+<<<<<<< HEAD
+<<<<<<< HEAD
+);
+=======
+);
+>>>>>>> master
+=======
+);
+>>>>>>> 320dbb085a3a993802e6cff5bc9015ced0a7bd27
